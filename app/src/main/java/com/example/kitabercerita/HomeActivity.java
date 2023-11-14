3892e85425e6c26427f105839beb5ac9d9808532
JuanGau <nathanael.gauthama@binus.ac.id>
2023-11-14 15:36:36 +0700

    Add menu option, home layout, link post recycler view

--- app/src/main/java/com/example/kitabercerita/HomeActivity.java
+++ app/src/main/java/com/example/kitabercerita/HomeActivity.java
@@ -2,13 +2,21 @@ package com.example.kitabercerita;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.example.kitabercerita.adapter.PostAdapter;
+import com.example.kitabercerita.model.Post;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 import java.util.zip.Inflater;
 
 public class HomeActivity extends AppCompatActivity {
@@ -17,19 +25,15 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
         Intent intent = null;
-        switch(itemId){
-            case R.id.homeMenu:
-                intent = new Intent(this.getApplicationContext(), HomeActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.searchMenu:
-                intent = new Intent(this.getApplicationContext(), SearchPostActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.profileMenu:
-                intent = new Intent(this.getApplicationContext(), ProfileViewActivity.class);
-                startActivity(intent);
-                break;
+        if(itemId == R.id.homeMenu) {
+            intent = new Intent(this.getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+        }else if(itemId == R.id.searchMenu) {
+            intent = new Intent(this.getApplicationContext(), SearchPostActivity.class);
+            startActivity(intent);
+        }else if (itemId == R.id.profileMenu) {
+            intent = new Intent(this.getApplicationContext(), ProfileViewActivity.class);
+            startActivity(intent);
         }
         return true;
     }
@@ -41,9 +45,31 @@ public class HomeActivity extends AppCompatActivity {
         return true;
     }
 
+    FloatingActionButton gotoInsertPostBtn;
+    RecyclerView postView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        postView = findViewById(R.id.homePostView);
+        gotoInsertPostBtn = findViewById(R.id.gotoInsertPostBtn);
+
+        //go to insert post
+        gotoInsertPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, InsertPostActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //recycler view bind
+        ArrayList<Post> postList = new ArrayList<>();
+        //TODO: insert data to postList using Firebase
+
+        postView.setLayoutManager(new LinearLayoutManager(this));
+        postView.setAdapter(new PostAdapter(this, postList));
     }
 }
--- app/src/main/java/com/example/kitabercerita/ProfileViewActivity.java
+++ app/src/main/java/com/example/kitabercerita/ProfileViewActivity.java
@@ -1,10 +1,39 @@
 package com.example.kitabercerita;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ProfileViewActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        Intent intent = null;
+        if(itemId == R.id.homeMenu) {
+            intent = new Intent(this.getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+        }else if(itemId == R.id.searchMenu) {
+            intent = new Intent(this.getApplicationContext(), SearchPostActivity.class);
+            startActivity(intent);
+        }else if (itemId == R.id.profileMenu) {
+            intent = new Intent(this.getApplicationContext(), ProfileViewActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inf = getMenuInflater();
+        inf.inflate(R.menu.option_menu,menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

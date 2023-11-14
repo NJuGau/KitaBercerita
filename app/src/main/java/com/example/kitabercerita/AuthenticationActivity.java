074560c547b7603d7210326327551ca44ae2754d
JuanGau <nathanael.gauthama@binus.ac.id>
2023-11-14 13:54:59 +0700

    Add menu, activity search

--- app/src/main/java/com/example/kitabercerita/AuthenticationActivity.java
+++ app/src/main/java/com/example/kitabercerita/AuthenticationActivity.java
@@ -1,11 +1,42 @@
 package com.example.kitabercerita;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class AuthenticationActivity extends AppCompatActivity {
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        Intent intent = null;
+        switch(itemId){
+            case R.id.homeMenu:
+                intent = new Intent(this.getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.searchMenu:
+                intent = new Intent(this.getApplicationContext(), SearchPostActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.profileMenu:
+                intent = new Intent(this.getApplicationContext(), ProfileViewActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inf = getMenuInflater();
+        inf.inflate(R.menu.option_menu,menu);
+        return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

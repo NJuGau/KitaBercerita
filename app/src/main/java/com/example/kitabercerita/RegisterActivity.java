fff4710286e45e6da351bb610711e15a95abddb8
DarkAce341 <daveboy7dt@gmail.com>
2023-12-01 17:05:04 +0700

    Logika login dan register sudah

--- app/src/main/java/com/example/kitabercerita/RegisterActivity.java
+++ app/src/main/java/com/example/kitabercerita/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.kitabercerita;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +13,7 @@ import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText Email, Password;
+    private EditText Email, Password, Username, Phonenumber;
     private  Button buttonReg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,8 @@ public class RegisterActivity extends AppCompatActivity {
         Email = findViewById(R.id.editTextTextEmailAddress);
         Password = findViewById(R.id.editTextTextPassword);
         buttonReg = findViewById(R.id.buttonReg);
+        Username = findViewById(R.id.Username);
+        Phonenumber = findViewById(R.id.PhoneNumber);
 
         buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,20 +32,23 @@ public class RegisterActivity extends AppCompatActivity {
                 // Handle sign-up button click
                 String email = Email.getText().toString();
                 String password = Password.getText().toString();
-                if (isValidRegister(email, password)) {
+                String username = Username.getText().toString();
+                String phoneNumber = Phonenumber.getText().toString();
+                if (isValidRegister(username, email, password, phoneNumber)) {
                     // Successful sign-up
                     Toast.makeText(RegisterActivity.this, "Sign Up Successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                    startActivity(intent);
                 } else {
                     // Failed sign-up
                     Toast.makeText(RegisterActivity.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
                 }
+
             }
             //TODO: upload data to firebase
         });
     }
-    private boolean isValidRegister(String email, String password) {
-        // Implement your sign-up logic here
-        // For simplicity, we'll consider it a valid sign-up if username and password are not empty
-        return !email.isEmpty() && !password.isEmpty();
+    private boolean isValidRegister(String username, String email, String password, String phoneNumber) {
+        return !username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !phoneNumber.isEmpty();
     }
 }

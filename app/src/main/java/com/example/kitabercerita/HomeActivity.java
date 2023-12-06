89723946f9f864923aa648f10eb40bb4fcf93ecf
JuanGau <nathanael.gauthama@binus.ac.id>
2023-12-07 01:27:27 +0700

    Insert to post, comment, get from post

--- app/src/main/java/com/example/kitabercerita/HomeActivity.java
+++ app/src/main/java/com/example/kitabercerita/HomeActivity.java
@@ -7,23 +7,35 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.kitabercerita.adapter.ClickListener;
 import com.example.kitabercerita.adapter.PostAdapter;
 import com.example.kitabercerita.model.Post;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 import java.util.zip.Inflater;
 
 public class HomeActivity extends AppCompatActivity implements ClickListener {
 
     private ArrayList<Post> postList;
+    FirebaseDatabase db;
+    DatabaseReference rf;
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
@@ -60,7 +72,6 @@ public class HomeActivity extends AppCompatActivity implements ClickListener {
         postView = findViewById(R.id.homePostView);
         gotoInsertPostBtn = findViewById(R.id.gotoInsertPostBtn);
 
-//        FirebaseAuth.getInstance().signInWithEmailAndPassword("test@gmail.com", "test123");
 
         //go to insert post
         gotoInsertPostBtn.setOnClickListener(new View.OnClickListener() {
@@ -74,6 +85,28 @@ public class HomeActivity extends AppCompatActivity implements ClickListener {
         //recycler view bind
         postList = new ArrayList<>();
         //TODO: insert data to postList using Firebase
+
+        db = FirebaseDatabase.getInstance("https://mobile-78ad2-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        rf = db.getReference().child("Post");
+
+        rf.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d("Status", "retrieving data..." + snapshot.getValue().toString());
+                for(DataSnapshot sn : snapshot.getChildren()){
+//                    HashMap<String, Object> map = (HashMap<String, Object>) sn.getValue();
+                    Log.d("Status", "this is getting data...." + sn.getValue().toString());
+                    Log.d("post Id", sn.getKey().toString());
+                    Log.d("description", sn.child("postDescription").getValue(String.class));
+                    Log.d("user", sn.child("postUserId").getValue(String.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(HomeActivity.this, "FAILED", Toast.LENGTH_SHORT);
+            }
+        });
 
         postList.add(new Post("129031209381293","lololol" +
                 "xcxzczxczxczxczxczxc" +
